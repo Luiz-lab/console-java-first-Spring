56563785e90572a012a78b6bed3ddff936ed481a
Luiz-lab <calixtoluiz0@gmail.com>
2022-09-03 19:15:20 -0300

    Exercicio2

--- src/main/java/com/br/calixto/FirstStep/FirstStepApplication.java
+++ src/main/java/com/br/calixto/FirstStep/FirstStepApplication.java
@@ -14,38 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FirstStepApplication {
 
 	public static void main(String[] args) {
-		// Programa de reprodução de nomes;
-		// System.out.println("Meu Primeiro Projeto em SpringBoot, top!!!");
-		// Scanner teclado = new Scanner(System.in);
-		// System.out.println("DIgite o primeiro nome: ");
-		// String nome1 = teclado.nextLine();
-		// System.out.println("DIgite o segundo nome: ");
-		// String nome2 = teclado.nextLine();
-		// System.out.println("DIgite o terceiro nome: ");
-		// String nome3 = teclado.nextLine();
-		// System.out.println("DIgite o quarto nome: ");
-		// String nome4 = teclado.nextLine();
-		// System.out.println("DIgite o quinto nome: ");
-		// String nome5 = teclado.nextLine();
-		// System.out.println("Os nomes digitados foram: "+ nome1 +","+nome2+","+nome3+","+nome4+","+nome5 );
-		// String [] nomes;
-		// nomes = new String[5];
-		// for (int i =1;i<=5;i++){
-		// 	System.out.println("Digite o nome "+i+": ");
-		// 	nomes[i] = teclado.nextLine();
-		// }
-		// System.out.println(Arrays.toString(nomes));
-		// List<String> nomenclaturas = new ArrayList<>();
-		// for(int i =1;i<=5;i++){
-		// 	System.out.println("Digite o nome "+i+": ");
-		// 	String setnomes = teclado.nextLine();
-		// 	nomenclaturas.add(setnomes);
-		// }
-		// for (String setString : nomenclaturas) {
-		// 	System.out.println("Os nomes digitado foi:"+setString );
-		// }
 		
-
 		SpringApplication.run(FirstStepApplication.class, args);
 	}
 }
